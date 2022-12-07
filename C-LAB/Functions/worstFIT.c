9a0a02ffc7ac0df19c0c3636ffb1b180f0791be3
RajatSandeep <rajatsandeepsen1839@gmail.com>
2022-12-07 22:36:01 +0530

    worst fit done

---
+++ C-LAB/Functions/worstFIT.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+void bestFit(int block[], int blockSIZE, int process[], int processSIZE)
+{
+    for (int i = 0; i < processSIZE; i++)
+    {
+        int bestIdx = -1;
+        for (int j = 0; j < blockSIZE; j++)
+        {
+            if (block[j] >= process[i])
+            {
+                if (bestIdx == -1)
+                    bestIdx = j;
+                else if (block[bestIdx] < block[j])
+                    bestIdx = j;
+            }
+        }
+
+        if (bestIdx != -1)
+        {
+            printf("Process %d (%dKB) allocated at %dKB Memory Space\n", i + 1, process[i], block[bestIdx]);
+            block[bestIdx] -= process[i];
+            printf("Remaining space is : %d\n\n", block[bestIdx]);
+        }
+        else
+            printf("Process %d (%dKB) is not allocated\n\n", i + 1, process[i]);
+    }
+}
+
+int main()
+{
+    int block[] = {100, 500, 200, 300, 600};
+    int process[] = {400, 417, 112, 626};
+    int m = sizeof(block) / sizeof(block[0]);
+    int n = sizeof(process) / sizeof(process[0]);
+
+    bestFit(block, m, process, n);
+}
