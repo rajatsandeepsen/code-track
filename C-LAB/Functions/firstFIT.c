fe3986212fc2e8eb0719a223080c3474fe449b5a
RajatSandeep <rajatsandeepsen1839@gmail.com>
2022-12-07 22:32:53 +0530

    first fit done

--- C-LAB/Functions/firstFIT.c
+++ C-LAB/Functions/firstFIT.c
@@ -3,33 +3,30 @@ void bestFit(int block[], int blockSIZE, int process[], int processSIZE)
 {
     for (int i = 0; i < processSIZE; i++)
     {
-        int bestIdx = -1;
+        int flag = 0;
         for (int j = 0; j < blockSIZE; j++)
         {
             if (block[j] >= process[i])
             {
-                if (bestIdx == -1)
-                    bestIdx = j;
-                else if (block[bestIdx] > block[j])
-                    bestIdx = j;
+                printf("Process %d (%dKB) allocated at %dKB Memory Space\n", i + 1, process[i], block[j]);
+                block[j] -= process[i];
+                printf("Remaining space is : %d\n\n", block[j]);
+                flag = 1;
+                break;
             }
         }
 
-        if (bestIdx != -1)
+        if (flag != 1)
         {
-            printf("Process %d (%dKB) allocated at %dKB Memory Space\n", i + 1, process[i], block[bestIdx]);
-            block[bestIdx] -= process[i];
-            printf("Remaining space is : %d\n\n", block[bestIdx]);
-        }
-        else
             printf("Process %d (%dKB) is not allocated\n\n", i + 1, process[i]);
+        }
     }
 }
 
 int main()
 {
     int block[] = {100, 500, 200, 300, 600};
-    int process[] = {212, 417, 112, 626};
+    int process[] = {212, 417, 112, 600};
     int m = sizeof(block) / sizeof(block[0]);
     int n = sizeof(process) / sizeof(process[0]);
 
